751129a10b12230d057951e45ddb7b581d072d2d
Henry <henry.webel@cpr.ku.dk>
2023-10-30 14:36:47 +0100

    :bug: import databases failed

---
+++ test/test_pkg.py
@@ -0,0 +1,15 @@
+
+
+def test_pkg_imports():
+    import hela_data
+    import hela_data.io
+    import hela_data.io.mq
+    import hela_data.plotting
+    import hela_data.pandas
+    import hela_data.utils
+    import hela_data.log
+    import hela_data.fasta
+    import hela_data.analyzers
+    import hela_data.analyzers.analyzers
+    import hela_data.data_handling
+    import hela_data.databases.uniprot
